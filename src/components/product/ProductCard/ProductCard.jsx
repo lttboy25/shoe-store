a7f1e9b09f60c206d0ccc3f0f16ac2ec6706db04
lttboy25 <139521084+lttboy25@users.noreply.github.com>
2026-02-09 08:53:19 +0700

    feat(productCard, productGrid): create Grid and update Card

---
+++ src/components/product/ProductCard/ProductCard.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Promotion from "../../common/Promotion";
+import "./ProductCard.css";
+import likeIcon from "../../../assets/icon/heart-like.svg";
+import unlikeIcon from "../../../assets/icon/heart-un-like.svg";
+
+function ProductCard({ product }) {
+  const [like, setLike] = useState(false);
+
+  function handleLike() {
+    setLike(!like);
+    console.log(like);
+  }
+
+  return (
+    <div className="product-card">
+      <div className="product-image-wrapper">
+        <img
+          src={product.thumbnail}
+          alt={product.slug}
+          className="product-image"
+        />
+
+        <button
+          onClick={handleLike}
+          className="product-like-btn"
+        >
+          <img
+            src={like ? likeIcon : unlikeIcon}
+            alt="Like"
+            className="product-like-icon"
+          />
+        </button>
+      </div>
+
+      <div className="product-name-wrapper">
+        <h5 className="product-name">{product.name}</h5>
+      </div>
+
+      <div className="product-price">
+        <span className="product-original-price">
+          {product.originalPrice} đ
+        </span>
+        <span className="product-sale-price">
+          {product.price} đ
+        </span>
+        <Promotion title={`${product.discountPercent}`} />
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
